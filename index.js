c69526c286a3d05b8e91ab101c91403d462deafd
evgene30 <evgene.fe@gmail.com>
2021-11-08 17:02:52 +0300

    add to JSON

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.get('/api', (req, res) => {
     client.connect(async (err) => {
         try {
-            res.send(await collection.find({}).toArray());
+            res.send(await collection.find({}).toJSON());
             await client.close();
         } catch (err) {
             console.log(err);
@@ -29,8 +29,6 @@ app.post('/post', urlencodedParser, (req, res) => {
     if (!req.body) return res.sendStatus(400);
     methodsDB.setUsers(JSON.parse(req.body.user));
     res.send('Your register!');
-
-
 });
 
 app.delete('/del', urlencodedParser, (req, res) => {
