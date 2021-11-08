275f7745944a3d4cb18d9098396f563d0932ac88
evgene30 <evgene.fe@gmail.com>
2021-11-08 15:30:41 +0300

    add corse

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const urlencodedParser = bodyParser.urlencoded({
 })
 const {client, collection, methodsDB} = require('./mongoDB/mongoConnect');
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.get('/api', (req, res) => {
     client.connect(async (err) => {
